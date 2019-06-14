b0054603cb7a18a03720074a7a33eb291e187734
dj0nny <frank96990@gmail.com>
2019-06-14 16:38:08 +0200

    :yum: add type badges, :pencil2: edit PkmnDetail.vue

--- src/views/PkmnDetail.vue
+++ src/views/PkmnDetail.vue
@@ -1,32 +1,54 @@
 <template>
-  <div class="detail">
+  <div class="details">
     <div class="container">
-      <h1 class="name">{{ pkmnDetail.name }}<br></h1>
-      <h3>Pokedex number: <small>{{ pkmnDetail.id }}</small></h3> 
-      
 
+      <h1 class="name">{{ pkmnDetail.name }}</h1>
+
+  
       <div class="row">
-
-        <div class="col-md-8">
-          <!-- <div class="row">
-            <div class="col-md-4" v-for="sprite in pkmnDetail.sprites" :key="sprite">
-              <div v-if="sprite != null">
-                <img :src="sprite">
+        <div class="col-md-12">
+          <div class="general-details">
+            <h3>Details</h3>
+            <div class="row">
+              <div class="col-md-4">
+                <h6>Pokèdex id:</h6>
+                {{ pkmnDetail.id }}
               </div>
+              <div class="col-md-4">
+                <h6>Abilities:</h6>
+
+              </div>
+              <div class="col-md-4">
+                <h6>Types:</h6>
+              </div>
+
             </div>
-          </div> -->
+          </div>
         </div>
 
-        <div class="col-md-4">
-          <h3 class="my-3">Project Description</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-          <h3 class="my-3">Project Details</h3>
-          <ul>
-            <li>Lorem Ipsum</li>
-            <li>Dolor Sit Amet</li>
-            <li>Consectetur</li>
-            <li>Adipiscing Elit</li>
-          </ul>
+        <div class="col-md-12">
+          <div class="sprites">
+            <h3>Sprites</h3>
+            <div class="table-responsive">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th v-for="type in spriteTypes" :key="type">{{type}}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td v-for="sprite in pkmnDetail.sprites" :key="sprite" :class="{ noSprite: !sprite }">
+                      <div class="placeholder">
+                        -
+                      </div>
+                      <img :src="sprite">
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
 
       </div>
@@ -46,6 +68,7 @@ import * as API from '@/api'
 @Component
 export default class PkmnDetail extends Vue {
   pkmnDetail?: PkmnResponse[] = []
+  spriteTypes: string[] = ["back default", "back female", "back shiny", "back shiny female", "front default", "front female", "front shiny", "front shiny female"]
 
   async created() {
     await pkmn.getDetail(this.$route.params.name).then(() => {
@@ -58,13 +81,43 @@ export default class PkmnDetail extends Vue {
 
 
 <style lang="scss" scoped>
-.detail {
+.details {
   margin-top: 30px;
   margin-bottom: 30px;
 
   h1.name {
     margin-bottom: 20px;
     text-transform: capitalize;
+  }
+
+  h3 {
+    margin-bottom: 25px;
+  }
+
+  .general-details {
+    margin-top: 25px;
+  }
+
+  .sprites {
+    margin-top: 25px;
+    margin-bottom: 25px;
+
+      h3 {
+        margin-bottom: 20px;
+      }
+
+      table.table th {
+        text-transform: capitalize;
+      }
+
+      .placeholder {
+        display: none;
+      }
+      
+      .noSprite .placeholder {
+        display: block;
+        text-align: center;
+      }
   }
 }
 
