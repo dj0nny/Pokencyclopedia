0d3499db655b51892280fe1bb8a0fa4ab6b44654
dj0nny <frank96990@gmail.com>
2019-07-06 10:22:51 +0200

    :pencil2: edit MovesList.vue

--- src/components/MovesList.vue
+++ src/components/MovesList.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-    {{moves}}
+  <div class="move-list-table">
+    <div class="table-responsive">
+      <table class="table">
+        <!-- {{moves}} -->
+        <thead>
+          <tr>
+            <th scope="col">Move name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in moves" :key="index">
+            <!-- {{item.version_group_details}} -->
+            <td class="pokemon-name">{{ item.move.name }}</td>
+            <td class="pokemon-name" v-for="(item2, index2) in item.version_group_details" :key="index2">{{ item2 }}</td>
+            <!-- <td class="is-hidden"><b-form-checkbox :checked="item.is_hidden" disabled></b-form-checkbox></td> -->
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
